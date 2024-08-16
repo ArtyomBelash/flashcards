4f38bece87d45097268ed8d2223550ab82a6cc7f
arty <arteniy999@gmail.com>
2024-08-16 14:25:57 +0300

    updated

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 
 from api.permissions import IsOwnerOrAdminOnly
 from api.serializers import CardSerializer
@@ -10,9 +10,13 @@ from cards.models import Card
 class CardViewSet(viewsets.ModelViewSet):
     serializer_class = CardSerializer
     permission_classes = (IsOwnerOrAdminOnly, IsAuthenticated,)
+    pagination_class = LimitOffsetPagination
 
     def get_queryset(self):
+        if self.request.user.is_superuser:
+            return Card.objects.all()
         if self.request.user.is_authenticated:
             return Card.objects.filter(owner=self.request.user)
+
 
 
